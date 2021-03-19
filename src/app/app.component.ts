ed885e834bcd6a962363e33b68e4f15dbe23a7a9
FacuGauto <fgauto1992@gmail.com>
2021-03-18 21:03:44 -0300

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'primer-proyecto';
+
+  public edadUno;
+  public edadDos;
+  public resultadoPromedio;
+  public resultadoSuma;
+
+  promedio() {
+    this.resultadoPromedio = (Number(this.edadUno) + Number(this.edadDos))/2;
+  }
+
+  suma() {
+    this.resultadoSuma = (Number(this.edadUno) + Number(this.edadDos));
+  }
+
+  calcular() {
+    this.suma();
+    this.promedio();
+  }
+
+  limpiar() {
+    this.edadUno = "";
+    this.edadDos = "";
+    this.resultadoPromedio = "";
+    this.resultadoSuma = "";
+  }
+}
